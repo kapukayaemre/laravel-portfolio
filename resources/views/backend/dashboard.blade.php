00af92d284bdb6a5b1bada1034cdcbc18f6c929a
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-08-13 05:00:10 +0300

    Folder structure updated

---
+++ resources/views/backend/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends("layouts.admin")
+@section("title")
+    Dashboard
+@endsection
+
+@section("css")
+@endsection
+
+@section("content")
+    <div class="d-flex mx-auto">
+        <h1>Dashboard</h1>
+    </div>
+@endsection
+
+@section("js")
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('backend.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
