aee854ebcb11e428ce46c62b1667b6f640e65177
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-08-14 17:58:43 +0300

    frontend theme integrated and sections created

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,49 @@
+@extends("layouts.front")
+
+@section("title")
+    Anasayfa
+@endsection
+
+@section("css")
+@endsection
+
+@section("content")
+    <!-- Header-Area-Start -->
+    @include("frontend.sections.hero")
+    <!-- Header-Area-End -->
+
+    <!-- Service-Area-Start -->
+    @include("frontend.sections.service")
+    <!-- Service-Area-End -->
+
+    <!-- About-Area-Start -->
+    @include("frontend.sections.about")
+    <!-- About-Area-End -->
+
+    <!-- Portfolio-Area-Start -->
+    @include("frontend.sections.portfolio")
+    <!-- Portfolio-Area-End -->
+
+    <!-- Skills-Area-Start -->
+    @include("frontend.sections.skills")
+    <!-- Skills-Area-End -->
+
+    <!-- Experience-Area-Start -->
+    @include("frontend.sections.experience")
+    <!-- Experience-Area-End -->
+
+    <!-- Testimonial-Area-Start -->
+    @include("frontend.sections.testimonial")
+    <!-- Testimonial-Area-End -->
+
+    <!-- Blog-Area-Start -->
+    @include("frontend.sections.blog")
+    <!-- Blog-Area-End -->
+
+    <!-- Contact-Area-Start -->
+    @include("frontend.sections.contact")
+    <!-- Contact-Area-End -->
+@endsection
+
+@section("js")
+@endsection
